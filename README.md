bbd41ef667bdad9053913cd48031789adde33188
Hiroaki Suzuki <rirazou05410@gmail.com>
2021-01-27 22:49:53 +0900

    create tool.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# xml-file-format-tool
-Format xml file
+# XML File Format Tool
+
+## synopsis
+
+This tool is a command line tool that formats XML files under a specified directories.  
+The file traversal is done recursively.
+
+## usage
+
+```bat
+xml-format-tool.exe "path1" "path2"
+```
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"github.com/go-xmlfmt/xmlfmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var duplicateLineBreakRep = regexp.MustCompile(`(\s*?\r?\n)+`)
+var firstLineLineBreakRep = regexp.MustCompile(`(^\r?\n)+`)
+var endLineLineBreakRep = regexp.MustCompile(`(\r?\n$)+`)
+
+func main() {
+	flag.Parse()
+	paths := flag.Args()
+
+	for _, rootPath := range paths {
+		fileInfos, err := ioutil.ReadDir(rootPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, fileInfo := range fileInfos {
+			if fileInfo.IsDir() {
+				format(rootPath, fileInfo)
+			}
+		}
+	}
+}
+
+func format(parentDirPath string, dirInfo os.FileInfo) {
+	dirPath := filepath.Join(parentDirPath, dirInfo.Name())
+	fileInfos, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fileInfo := range fileInfos {
+		if fileInfo.IsDir() {
+			format(dirPath, fileInfo)
+		} else if strings.HasSuffix(fileInfo.Name(), ".xml") {
+			targetFilePath := filepath.Join(dirPath, fileInfo.Name())
+			content, err := ioutil.ReadFile(targetFilePath)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			formattedXML := xmlfmt.FormatXML(string(content), "", "    ")
+			formattedXML = duplicateLineBreakRep.ReplaceAllString(formattedXML, "\r\n")
+			formattedXML = firstLineLineBreakRep.ReplaceAllString(formattedXML, "")
+			formattedXML = endLineLineBreakRep.ReplaceAllString(formattedXML, "")
+			if err = ioutil.WriteFile(targetFilePath, []byte(formattedXML), fileInfo.Mode().Perm()); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
